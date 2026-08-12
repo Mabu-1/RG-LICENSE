4c441819540005f318798031a402b705d04ad946
Mabu-1 <umahtab65@gmail.com>
2026-08-12 18:00:10 +0600

    admin: add useBranding hook

---
+++ app/admin/hooks/useBranding.js
@@ -0,0 +1,17 @@
+'use client'
+import { useState, useEffect } from 'react'
+import { supabaseBrowser } from '@/lib/supabase'
+
+export function useBranding() {
+  const [logo, setLogo]             = useState('')
+  const [logoHeight, setLogoHeight] = useState(44)
+
+  useEffect(() => {
+    supabaseBrowser.from('settings').select('value').eq('key','branding_logo').single()
+      .then(({ data }) => { if (data?.value) setLogo(data.value) })
+    supabaseBrowser.from('settings').select('value').eq('key','branding_logo_height').single()
+      .then(({ data }) => { if (data?.value) setLogoHeight(parseInt(data.value)) })
+  }, [])
+
+  return { logo, logoHeight }
+}
