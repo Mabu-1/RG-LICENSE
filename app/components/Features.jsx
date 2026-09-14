9e17f7fdaa9c6f0e7dfef1de07af520afd3cbc07
BrandsBro <mahtab.brandsbrollc@gmail.com>
2026-09-15 02:08:45 +0600

    added

--- app/components/Features.jsx
+++ app/components/Features.jsx
@@ -1,20 +1,12 @@
-import CardLayouts from './features/CardLayouts'
-import GoogleSheets from './features/GoogleSheets'
-import Lightbox from './features/Lightbox'
-import SearchFilter from './features/SearchFilter'
-import ThemeSettings from './features/ThemeSettings'
-
-
+import CardLayouts from "./features/CardLayouts";
+import ThemeSettings from "./features/ThemeSettings";
 
 export default function Features() {
   return (
     <>
-  
-      <GoogleSheets />
       <CardLayouts />
-      <Lightbox/>
-      <SearchFilter/>
-      <ThemeSettings/>
+
+      <ThemeSettings />
     </>
-  )
+  );
 }
